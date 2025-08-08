aed91ecc3f1a558b8fba68cf8564f40fcd8e8b21
KristofferStrube <KristofferStrube@users.noreply.github.com>
2025-08-08 12:27:49 +0000

    deploy: 32b6399c76edb3466724e2b984e362dc0e129bc9

---
+++ _content/KristofferStrube.Blazor.Popper/KristofferStrube.Blazor.popper.js
@@ -0,0 +1,57 @@
+﻿export function createPopper(reference, popper, options) {
+    cleanOptions(options);
+
+    if (!(reference instanceof Element)) {
+        var objRef = reference.objRef;
+        reference.getBoundingClientRect = () => objRef.invokeMethod('CallGetBoundingClientRect');
+        delete reference.objRef;
+    }
+
+    return Popper.createPopper(reference, popper, options);
+}
+
+export function getStateOfInstance(instance) {
+    return stripState(instance.state)
+}
+
+export function updateOnInstance(instance) {
+    return instance.update().then(state => stripState(state));
+}
+
+export function setOptionsOnInstance(instance, options) {
+    cleanOptions(options);
+
+    return instance.setOptions(options).then(state => stripState(state));
+}
+
+function stripState(state) {
+    return {
+        attributes: state.attributes,
+        elements: Object.keys(state.elements),
+        modifiersData: state.modifiersData,
+        placement: state.placement,
+        // We need a custom serilizer/deserilizer before we can parse this back.
+        // orderedModifiers: state.orderedModifiers.map(modifier => ({ name: modifier.name, enabled: modifier.enabled, phase: modifier.phase })),
+    }
+}
+
+function cleanOptions(options) {
+    options.onFirstUpdate = (state) => options.objRef.invokeMethodAsync('CallOnFirstUpdate', stripState(state));
+
+    if (options.modifiers != null) {
+        options.modifiers = options.modifiers.map(modifier => {
+            var objRef = modifier.objRef;
+            if (modifier.hasFn) {
+                modifier.fn = (modifierArguments) => {
+                    modifierArguments.state = stripState(modifierArguments.state);
+                    delete modifierArguments.instance;
+                    delete modifierArguments.options;
+                    objRef.invokeMethodAsync('CallFn', modifierArguments);
+                }
+            }
+            delete modifier.objRef;
+            delete modifier.hasFn;
+            return modifier;
+        });
+    }
+}
